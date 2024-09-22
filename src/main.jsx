8ffd462767be34c0c03f48cb8c71b9ff65a24e8d
PhaseOfficial <97903370+PhaseOfficial@users.noreply.github.com>
2024-09-22 10:19:08 +0200

    update

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,9 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-  <BrowserRouter basename="./">
+  <BrowserRouter basename="/redcupseries/">
   <Routes>
-      <Route path="*" element={<App/>}/>
+      <Route path="*" element={<App />}/>
   </Routes>
   </BrowserRouter>
   </React.StrictMode>,
